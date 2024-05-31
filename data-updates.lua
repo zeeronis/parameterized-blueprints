a30962ab02a435d7156e4116759291a3bf6758b2
zeeronis <zeeronis@gmail.com>
2024-05-31 14:52:56 +0300

    version 1.0.7 - fix productivity modules extensions; Add confict dependency to original mod.

--- data-updates.lua
+++ data-updates.lua
@@ -6,8 +6,10 @@ end
 
 for _, value in pairs(data.raw["module"]) do
     if value.name:find("productivity%-module") then
-        for i = 0, 9, 1 do
-            value.limitation[#value.limitation+1] = "placeholder-" .. i
+        if value.limitation then
+            for i = 0, 9, 1 do
+                value.limitation[#value.limitation+1] = "placeholder-" .. i
+            end
         end
-      end
+    end
 end
